ab12114477b9f6df9b4a61e75e485cb72675c8e7
sze4kalend <sze4kalend@vasvari.org>
2025-12-08 18:42:02 +0100

    vers kész,csv kiolvasás elkezdése

--- filegyakorlas1208.dart
+++ filegyakorlas1208.dart
@@ -1,1 +1,37 @@
+import "dart:io";
 
+void main(){
+  List<String> vers = fileOlvaso('vers.txt');
+  //versKiiratas(vers);
+  //harmadikVersszak(vers);
+  List<String> students = fileOlvaso('students_en.csv');
+  fizikasok(students);
+}
+
+void fizikasok(tanulok) {
+  
+}
+
+void harmadikVersszak(List<String> sorok) {
+  int hanyadikSor = 0;
+  for (var sor in sorok) {
+    if (sor.isEmpty) {
+      hanyadikSor++;
+      if (hanyadiksor == 4) {
+        break;
+      }
+    }
+  }
+}
+
+void versKiiratas(sorok) {
+  for (var sor in sorok) {
+    print(sor);
+  }
+}
+List<String> fileOlvaso(fileName) {
+  List<String> tartalom = [];
+  File file = File(fileName);
+  tartalom = file.readAsLinesSync();
+  return tartalom;
+}
